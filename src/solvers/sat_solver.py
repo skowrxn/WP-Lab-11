6f048cb1732e46c61f9f7981ae2f42a24d0b906f
Bartłomiej <bartomiejskowron@icloud.com>
2025-06-07 17:45:22 +0200

    Fix puzzle assignment logic to use explicit row and column indexing.

--- src/solvers/sat_solver.py
+++ src/solvers/sat_solver.py
@@ -144,11 +144,11 @@ class SudokuCNF:
         """
 
         valid_ids = [prop_id for prop_id in results if prop_id >= 0]
-        valid_propositions = [self.propositions[valid_prop_id] for valid_prop_id in valid_ids]
+        valid_propositions = [self.propositions[valid_prop_id] for valid_prop_id in valid_ids] 
         solved_puzzle = self.puzzle.copy()
 
         for valid_proposition in valid_propositions:
-            solved_puzzle.__setitem__(valid_proposition.coords, valid_proposition.val)
+            solved_puzzle[valid_proposition.coords.row, valid_proposition.coords.col] = valid_proposition.val
 
         return solved_puzzle
 
